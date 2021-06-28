c849acd939e67ad68b6e5335256e9efbafdb9513
Pavel Yakupov <pavel.yakupov@software-kit.ru>
2021-06-28 13:04:57 +0300

    hot fix

--- app/pages/product.vue
+++ app/pages/product.vue
@@ -3,27 +3,3 @@
     <h1>Привет это продукты</h1>
   </div>
 </template>
-<script>
-
-export default {
-data: function(){
-    products
-},
-methods:{
- async asyncData({ $axios, globalPage }) {
-    const photo = await $axios.$get(
-      `https://api.pexels.com/v1/curated?page=${globalPage}&per_page=24`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization:
-          key.KEY
-        },
-      }
-}
-}
-</script>
-
-<style lang="">
-
-</style>
